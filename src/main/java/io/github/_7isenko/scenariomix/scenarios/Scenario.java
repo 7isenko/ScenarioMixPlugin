6ac552259ddfdfffeac4c8504537bdfa720b6dc8
7isenko <7isenko.max@gmail.com>
2020-07-27 00:09:07 +0300

    runnable and listener system

--- src/main/java/io/github/_7isenko/scenariomix/scenarios/Scenario.java
+++ src/main/java/io/github/_7isenko/scenariomix/scenarios/Scenario.java
@@ -1,27 +1,39 @@
 package io.github._7isenko.scenariomix.scenarios;
 
+import io.github._7isenko.scenariomix.ScenarioMix;
+import io.github._7isenko.scenariomix.gui.ScenarioMenu;
+import org.bukkit.Bukkit;
 import org.bukkit.Material;
+import org.bukkit.event.HandlerList;
+import org.bukkit.event.Listener;
+import org.bukkit.scheduler.BukkitRunnable;
 
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
 public abstract class Scenario {
     private String name;
     private String[] description;
     private Material icon;
     private boolean started;
+    private Map<BukkitRunnable, Integer> runnables;
+    private Set<Listener> listeners;
 
     public Scenario(String name, String[] description, Material icon) {
         this.name = name;
         this.description = description;
         this.icon = icon;
         this.started = false;
+        // TODO: recreate runnable system, cancel and restart cause exception
+        runnables = new HashMap<>();
+        listeners = new HashSet<>();
     }
 
     public boolean enable() {
         if (started) return false;
         started = true;
         start();
+        startListeners();
+        startRunnables();
         return true;
     }
 
@@ -29,12 +41,38 @@ public abstract class Scenario {
         if (!started) return false;
         started = false;
         stop();
+        stopListeners();
+        stopRunnables();
         return true;
     }
 
     public abstract void start();
 
     public abstract void stop();
+
+    private void addRunnable(BukkitRunnable runnable, int period) {
+        runnables.put(runnable, period);
+    }
+
+    private void addListener(Listener listener) {
+        listeners.add(listener);
+    }
+
+    private void startListeners(){
+        listeners.forEach(listener -> Bukkit.getPluginManager().registerEvents(listener, ScenarioMix.plugin));
+    }
+
+    private void stopListeners(){
+        listeners.forEach(HandlerList::unregisterAll);
+    }
+
+    private void startRunnables(){
+        runnables.forEach((runnable, period) -> runnable.runTaskTimer(ScenarioMix.plugin, 20, period));
+    }
+
+    private void stopRunnables(){
+        runnables.forEach((runnable, integer) -> runnable.cancel());
+    }
 
     public String getName() {
         return name;
--- src/main/java/io/github/_7isenko/scenariomix/scenarios/snowballs/Snowballs.java
+++ src/main/java/io/github/_7isenko/scenariomix/scenarios/snowballs/Snowballs.java
@@ -6,12 +6,12 @@ import org.bukkit.Material;
 public class Snowballs extends Scenario {
 
     public Snowballs() {
-        super("Снежки", new String[]{"Снежки теперь наносят урон"}, Material.SNOW_BALL);
+        super("Снежки", new String[]{"Теперь только снежки", "могут наносить урон"}, Material.SNOW_BALL);
     }
 
     @Override
     public void start() {
-
+        // TODO: засчитывать фраги, убрать другие источники урона, дать всем 2 блока снега и тыкву
     }
 
     @Override
